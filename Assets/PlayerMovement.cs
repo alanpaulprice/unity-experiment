ec55b1e827c15b7351d75c59f3d0f3086e718800
Alan Price <alan.price@diverseinteractive.com>
2020-02-19 13:34:39 +0000

    add player movement script with forward movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Fixed update is used whenever physics are being manipulated
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 500 * Time.deltaTime);
+    }
+}
