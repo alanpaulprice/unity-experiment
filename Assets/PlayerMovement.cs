e8ab9079772c726b9290793c272eb6556221f3d0
Alan Price <alan.price@diverseinteractive.com>
2020-03-03 13:42:02 +0000

    add input handling

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,9 +4,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
     // Fixed update is used whenever physics are being manipulated
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 500 * Time.deltaTime);
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // User imput handling. Needs optimising
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
